4508a341730583181626ab04975cd9ae7d13d6e7
Daniel Iniguez <dainigue@ucsd.edu>
2020-02-27 01:20:00 -0800

    thirdpartyAPI lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,17 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'EwI1pnWObKJHiQ46VOABIYnVvVQ9ushg';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+  	center: [32.88, -117.236],
+  	// L.mapquest.tileLayer('map') is the MapQuest map tile layer
+  	layers: L.mapquest.tileLayer('map'),
+  	zoom: 12,
+  	zoomControl: false
+	});
+
+	//add a marker to the map
+	L.marker([32.88, -117.236]).addTo(map);
+
 }
